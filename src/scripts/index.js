258b81573b99ebe40ff9df615bddd9a5d04f1162
Raeesah <raeesahmoola529@gmail.com>
2025-09-19 11:00:42 +0200

    Create input and add to list button.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,19 @@ import "../styles/general.css";
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
+
+const container = document.getElementById("container");
+const input = document.createElement("input");
+input.type = "text";
+input.placeholder = "Add Task";
+const addBtn = document.createElement("button");
+addBtn.innerText = "Add";
+addBtn.addEventListener("click", () => {
+  const ul = document.createElement("ul");
+  const li = document.createElement("li");
+  li.innerText = input.value;
+  input.value = "";
+ container.append(ul);
+ul.appendChild(li);
+})
+container.append(input, addBtn);
