34d338d09fa872d726529bcb62cd10d56b1fcced
Raeesah <raeesahmoola529@gmail.com>
2025-09-19 13:14:58 +0200

    Add toggle functionality for checkbox. Add button for task popup.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ import "../styles/general.css";
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
-
+const wrapper = document.getElementById("popup");
 const container = document.getElementById("container");
 const taskUl = document.getElementById("tasks");
 const input = document.createElement("input");
@@ -14,7 +14,18 @@ addBtn.innerText = "Add";
 
 
 addBtn.addEventListener("click", () => {
-  const li = document.createElement("li");
+ const li = document.createElement("li");
+  const check = document.createElement("input");
+  check.type = "checkbox";
+  check.addEventListener("click", () => {
+    if(li.style.textDecoration === "line-through"){
+li.style.textDecoration = "none";
+li.style.opacity = "1";
+    }else{
+li.style.textDecoration = "line-through";
+li.style.opacity = "0.7";
+    }
+  })
   if(input.value.trim() === ""){
     input.placeholder = "Oops, Try Again!";
     addBtn.style.backgroundColor = "red";
@@ -32,12 +43,20 @@ deleteBtn.addEventListener("click", () => {
   li.remove();
 });
   input.value = "";
+  li.appendChild(check);
+
  taskUl.appendChild(li);
  li.appendChild(deleteBtn);
-
  
 });
 
 container.append(input, addBtn, taskUl);
 
 
+const addTask = document.createElement("button");
+addTask.innerText = "Add Task";
+addTask.addEventListener("click", () => {
+container.style.display = "block";
+
+})
+wrapper.append(addTask);
