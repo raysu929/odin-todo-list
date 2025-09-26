f870d4b274f467dc8f187031c5dc3923a008f49c
Raeesah <raeesahmoola529@gmail.com>
2025-09-26 12:45:35 +0200

    Create edit button for sidebar and add popup styles.

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -14,6 +14,10 @@ export function updateSidebar(projectName, projectContainerElement) {
 projPara.classList.add("projPara");
 entry.appendChild(projPara);
 
+ const edit = document.createElement("button");
+ edit.innerText = "✏️";
+ edit.classList.add("edit");
+ entry.appendChild(edit);
  const deleteBtn = document.createElement("button");
  deleteBtn.innerText = "🗑️";
  deleteBtn.classList.add("sidebar-delete");
@@ -24,6 +28,43 @@ entry.appendChild(projPara);
     if (projectContainerElement && projectContainerElement.remove) {
       projectContainerElement.remove();
     }
+  });
+  
+  edit.addEventListener("click", () => {
+    const overlay = document.createElement("div");
+    overlay.classList.add("taskOverlay"); 
+
+    const div = document.createElement("div");
+    div.classList.add("popupBox");
+    const editProj = document.createElement("h1");
+    editProj.innerText = "Edit Project";
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "New project name";
+    input.value = projPara.innerText;
+
+    const cancel = document.createElement("button");
+    cancel.innerText = "Cancel";
+    cancel.classList.add("cancel");
+    const submit = document.createElement("button");
+    submit.innerText = "Submit";
+    submit.classList.add("submit");
+    div.append(editProj, input, submit, cancel);
+    document.body.appendChild(div);
+    overlay.appendChild(div);
+    document.body.appendChild(overlay);
+
+    cancel.addEventListener("click", () => {
+      overlay.remove();
+    });
+
+    submit.addEventListener("click", () => {
+      const newName = input.value.trim();
+      if (newName) {
+        projPara.innerText = newName;
+      }
+      overlay.remove();
+    });
   });
   sidebar.appendChild(entry);
 }
